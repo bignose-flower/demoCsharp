372241aab8662bd3df25179eada326e42d8e7fa7
Tak <takutikapom@gmail.com>
2023-02-21 23:18:16 +0900

    first commit

---
+++ DemoWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DemoWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenDialog = new System.Windows.Forms.Button();
+            this.txtSelectedFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpenDialog
+            // 
+            this.btnOpenDialog.Location = new System.Drawing.Point(13, 13);
+            this.btnOpenDialog.Name = "btnOpenDialog";
+            this.btnOpenDialog.Size = new System.Drawing.Size(100, 23);
+            this.btnOpenDialog.TabIndex = 0;
+            this.btnOpenDialog.Text = "ファイルの選択";
+            this.btnOpenDialog.UseVisualStyleBackColor = true;
+            this.btnOpenDialog.Click += new System.EventHandler(this.btnOpenDialog_Click);
+            // 
+            // txtSelectedFile
+            // 
+            this.txtSelectedFile.Location = new System.Drawing.Point(13, 65);
+            this.txtSelectedFile.Name = "txtSelectedFile";
+            this.txtSelectedFile.Size = new System.Drawing.Size(494, 19);
+            this.txtSelectedFile.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "選択されたファイル";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Location = new System.Drawing.Point(431, 293);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSendFile.TabIndex = 3;
+            this.btnSendFile.Text = "送信";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(16, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "失敗";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(519, 328);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSelectedFile);
+            this.Controls.Add(this.btnOpenDialog);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOpenDialog;
+        private System.Windows.Forms.TextBox txtSelectedFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button btnSendFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DemoWindowsFormsApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenDialog_Click(object sender, EventArgs e)
+        {
+            //ダイアログボックスの表示
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK )
+            {
+                //選択されたファイルをテキストボックスに表示する
+                txtSelectedFile.Text = openFileDialog2.FileName;
+            }
+        }
+
+        private void btnSendFile_Click(object sender, EventArgs e)
+        {
+            string url = string.Format("http://localhost:8080/students/upload");
+            var content = new MultipartFormDataContent();
+            var fileName = txtSelectedFile.Text;
+            var fileContent = new StreamContent(File.OpenRead(fileName));
+            fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment")
+            {
+                FileName = Path.GetFileName(fileName)
+            };
+
+            //ファイル和文繰り返す
+            content.Add(fileContent);
+
+            var httpClient = new HttpClient();
+            HttpResponseMessage response = httpClient.PostAsync(url, content).Result;
+            Boolean status = response.IsSuccessStatusCode;
+            if (status)
+            {
+                label4.Text = "OK";
+                label4.ForeColor = System.Drawing.Color.Blue;
+                label4.Visible = true;
+            }
+            else
+            {
+                label4.Text = "NG";
+                label4.ForeColor = System.Drawing.Color.Red;
+                label4.Visible = true;
+            }
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
